702e842e5b2329acb8e651dec61369f9365c9451
garlicgarrison <60302232+garlicgarrison@users.noreply.github.com>
2021-04-20 22:08:53 -0700

    updated create user

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,7 +6,7 @@
 
 const functions = require('firebase-functions');
 const admin = require("firebase-admin");
-const express = require("express");``
+const express = require("express"); ``
 
 admin.initializeApp()
 /*
@@ -15,29 +15,29 @@ databaseURL: "https://alimentation-851c0-default-rtdb.firebaseio.com"
 */
 const db = admin.firestore();
 
-const addUserDoc =(user) => {
+const addUserDoc = (user) => {
     return db.collection("users").doc(user.uid).set({
-       bankaccount: null,
-       credit_card: null,
-       email: user.email,
-       main_address: {
-           address1: null,
-           address2: null,
-           city: null,
-           name: null,
-           state: null,
-           zip: null
-       },
-       name: {
-           first: user.displayName,
-           middle: null,
-           last: null
-       },
-       other_addresses: [],
-       password: null,
-       phone_number: null,
-       profile_pic_url: user.photoURL
-   })
+        bankaccount: null,
+        credit_card: { nameOnCard: "", number: "", expiry: null, cvc: "" },
+        email: user.email,
+        main_address: {
+            address1: null,
+            address2: null,
+            city: null,
+            name: null,
+            state: null,
+            zip: null
+        },
+        name: {
+            first: user.displayName,
+            middle: null,
+            last: null
+        },
+        other_addresses: [],
+        password: null,
+        phone_number: null,
+        profile_pic_url: user.photoURL
+    })
 }
 
 const addCustomer = async (user) => {
@@ -46,7 +46,7 @@ const addCustomer = async (user) => {
         favorite_stores: [],
         previous_bought_stores: []
     })
-    
+
     return db.collection("users").doc(user.uid).collection("customer").get().then(snapshot => {
         snapshot.forEach(customerDoc => {
             customerDoc.ref.collection("shopping_cart").doc().set({
@@ -82,19 +82,19 @@ exports.processSignUp = functions.auth.user().onCreate(async user => {
 const SALES_TAX = 0.09;
 const DELIVERY_FEE = 10;
 
-exports.shopCartCalc = 
-functions.firestore.document('users/{userId}/customer/{customerId}/shopping_cart/{shoppingCartId}')
-    .onWrite((change, context)=> {
-        let totalCost = 0;
-        change.after.data().items.forEach(item => {
-            totalCost += item.price * parseInt(item.quantity.amount)
+exports.shopCartCalc =
+    functions.firestore.document('users/{userId}/customer/{customerId}/shopping_cart/{shoppingCartId}')
+        .onWrite((change, context) => {
+            let totalCost = 0;
+            change.after.data().items.forEach(item => {
+                totalCost += item.price * parseInt(item.quantity.amount)
+            })
+            let tax = totalCost * SALES_TAX;
+            totalCost = tax + totalCost + DELIVERY_FEE;
+            change.after.ref.set({
+                delivery: DELIVERY_FEE,
+                tax: tax,
+                total_cost: totalCost
+            }, { merge: true })
+
         })
-        let tax = totalCost * SALES_TAX;
-        totalCost = tax + totalCost + DELIVERY_FEE;
-        change.after.ref.set({
-            delivery: DELIVERY_FEE,
-            tax: tax,
-            total_cost: totalCost
-        }, {merge: true})
-        
-    })
